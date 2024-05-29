fe951dde18777150876eeb58a016a1c0e8f43994
mrmbs1 <mbilalsadiq.19@gmail.com>
2024-05-29 16:49:48 +0100

    Remove 100 html code line and create new grid box using javascript

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-const scoreCounter = document.querySelector('.score');
-console.log(scoreCounter);
+const scoreCounter = document.querySelector('.score-counter');
+const grid = document.querySelector('.grid');
+const endGameScreen = document.querySelector('.end-game-screen');
+const endGameText = document.querySelector('.end-game-text');
+const playAgainButton = document.querySelector('.play-again');
+
+const totalCells = 100;
+const totalBombs = 90;
+const maxScore = 5;
+const bombsList =[];
+
+let score = 0;
+
+
+for (let i = 1; i <= 100; i++) {
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    grid.appendChild(cell);
+}
