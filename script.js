420a0b428e9065136a0138e381fd2fe092a95914
mrmbs1 <mbilalsadiq.19@gmail.com>
2024-05-29 17:08:44 +0100

    Loop created to randomly generate 5 numbers

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const endGameText = document.querySelector('.end-game-text');
 const playAgainButton = document.querySelector('.play-again');
 
 const totalCells = 100;
-const totalBombs = 90;
+const totalBombs = 5;
 const maxScore = 5;
 const bombsList =[];
 
@@ -15,5 +15,20 @@ let score = 0;
 for (let i = 1; i <= 100; i++) {
     const cell = document.createElement('div');
     cell.classList.add('cell');
+
+    cell.addEventListener('click', function () {
+        
+    });
+
     grid.appendChild(cell);
+    
+}
+
+while (bombsList.length < totalBombs) {
+    // Generate a random number between 1 and 100
+    const randomNumber = Math.floor(Math.random() * totalCells) + 1;
+    
+    if (bombsList.includes(randomNumber)){
+    bombsList.push(randomNumber);
+    }
 }
